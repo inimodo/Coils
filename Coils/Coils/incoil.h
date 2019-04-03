ef3b8c9b780ad3c3e4b05d3c37779af9af183647
Markus <46501910+inimodo@users.noreply.github.com>
2019-04-03 19:38:52 +0200

    3

--- Coils/Coils/incoil.h
+++ Coils/Coils/incoil.h
@@ -1,1 +1,30 @@
 #pragma once
+
+#define _C_POINT c_point
+typedef struct {
+	double d_t;
+	double d_i;
+	double d_u;
+	double d_ul;
+	double d_ur;
+}_C_POINT;
+
+#define _C_PERIOD c_period
+typedef struct {
+	float f_time;
+	float f_voltage;
+}_C_PERIOD;
+
+#define _C_SAMPLE c_sample 
+typedef struct {
+	float f_resistor;
+	float f_inductor;
+	int i_samples;
+	_C_PERIOD * cp_samples;
+	
+}_C_SAMPLE;
+
+#define _P_FILE_HEADER "[R %f #L %f #P %d]\n"
+#define _P_FILE_BODY "(V %f |T %f / %f)\n"
+int getperiodinfo(char* c_file, _C_SAMPLE* cs_samples);
+int getdata_a(_C_SAMPLE cs_sample, unsigned short us_periods, unsigned short us_upscale, _C_POINT ** p_dataset, int * i_setlength);
--- Coils/Coils/main.c
+++ Coils/Coils/main.c
@@ -16,19 +16,21 @@
 
 
 
+#define _P_GRAPHZERO	(2*_P_HEIGHT_S/4)
 
-#define _P_GRAPHZERO_AMPERE (_P_HEIGHT_S/4)
-#define _P_GRAPHZERO_LOAD	(2*_P_HEIGHT_S/4)
-#define _P_GRAPHZERO_INDUCTOR (3*_P_HEIGHT_S/4)
 
-#define _P_GRAPHCOLOR_ZEROLINE (_CPCD_COLOR){55,255,255}
+#define _P_GRAPHCOLOR_ZEROLINE (_CPCD_COLOR){0,0,255}
+#define _P_GRAPHCOLOR_POINTS (_CPCD_COLOR){255,0,0}
+#define _P_GRAPHCOLOR_BACKCOLOR (_CPCD_COLOR){0,0,0}
 
 static unsigned short us_periods = 1;
 static unsigned short us_upscale = 1;
 
+static unsigned float us_draw_scale_y = 1000.f;
+static unsigned float us_draw_scale_x = 5000.f;
 
-static float us_indmulti = 1;
-static float us_resmulti = 1;
+static _C_SAMPLE cs_loadedsample;
+
 
 static _CPCD_SPRITE s_graphwindow;
 static _CPCD_MAP m_map;
@@ -55,7 +57,10 @@ void editidle(_CPCD_SPRITE * s_look);
 void initelements();
 int callupdate();
 
-_CPCD_MAIN{
+_CPCD_MAIN
+{
+	getperiodinfo("H:\\VisualStudio\\Projekts\\C\\Coils\\GIT\\Coils\\Coils\\Coils\\infofile.txt",&cs_loadedsample);
+
 	initelements();
 	m_map = _CPCD_CREATE(_P_WIDTH,_P_HEIGHT);
 	s_graphwindow = _CPCD_SPR_CREATE(_P_WIDTH, _P_HEIGHT_S,NULL);
@@ -65,47 +70,64 @@ _CPCD_MAIN{
 	};
 	return 0;
 }
-_CPCD_UPDATE{
-	_CPCD_CLEAR(&m_map,(_CPCD_COLOR) { 0,0,0 });
+
+_CPCD_UPDATE
+{
+	_CPCD_CLEAR(&m_map,_P_GRAPHCOLOR_BACKCOLOR);
 
 	callupdate();
+
 	_CPCD_DRAWSPRITE(&m_map, s_graphwindow, _CPCD_SBM_OVERRIDE, 0);
 
 	SM_ALIAS sm_string = SM_TOSTRING(us_periods);
-
 	_CPCD_DRAWSTRING(&m_map, sm_string.c_text, sm_string.i_length, v_intexts[0], 4, (_CPCD_COLOR) { 255, 255, 255 });
 	sm_string = SM_TOSTRING(us_upscale);
 	_CPCD_DRAWSTRING(&m_map, sm_string.c_text, sm_string.i_length, v_intexts[2], 4, (_CPCD_COLOR) { 255, 255, 255 });
-	sm_string = SM_SET("SCALE");
+	sm_string = SM_SET("REPEATS");
 	_CPCD_DRAWSTRING(&m_map, sm_string.c_text, sm_string.i_length, v_intexts[1], 4, (_CPCD_COLOR) { 255, 255, 255 });
-	sm_string = SM_SET("PERIODS");
+	sm_string = SM_SET("SCALE");
 	_CPCD_DRAWSTRING(&m_map, sm_string.c_text, sm_string.i_length, v_intexts[3], 4, (_CPCD_COLOR) { 255, 255, 255 });
 
 	catchtrigger();
+
 	_CPCD_DRAWBUFFER(&m_map);
-return 0;
+	return 0;
 }
-_CPCD_END{
+
+_CPCD_END
+{
 	_CPCD_DELETE(&m_map);
 	free(v_intexts);
 	free(b_buttonlist);
 }
-_CPCD_EVENTS{return &el_list;}
-void catchclick(_CPCD_EVENT e_event) {
+
+_CPCD_EVENTS
+{
+	return &el_list;
+}
+
+void catchclick(_CPCD_EVENT e_event) 
+{
 	if (e_event.USI_INDEX == 1)us_periods++;
 	if (e_event.USI_INDEX == 0 && us_periods>1)us_periods--;
 	if (e_event.USI_INDEX == 2 && us_upscale>1)us_upscale--;
 	if (e_event.USI_INDEX == 3)us_upscale++;
 	callupdate();
 }
+
 void catchhover(_CPCD_EVENT e_event) {}
-void catchtrigger() {
-	for (int i_index_element = 0; i_index_element < el_list.USI_ELEMENTS; i_index_element++){
+
+void catchtrigger()
+{
+	for (int i_index_element = 0; i_index_element < el_list.USI_ELEMENTS; i_index_element++)
+	{
 		_CPCD_ELEMENTRENDER(&m_map, es_styletest, el_list.E_ELEMENTS[i_index_element]);
 	}
 	_CPCD_DRAWBUFFER(&m_map);
 }
-void initelements() {
+
+void initelements() 
+{
 	b_buttonlist = (_CPCD_ELEMENT*)malloc(sizeof(_CPCD_ELEMENT)*_P_INCREMENT_ELEMENTS * 2);
 	v_intexts = (_CPCD_VECTOR*)malloc(sizeof(_CPCD_VECTOR)*_P_INCREMENT_ELEMENTS * 2);
 
@@ -155,28 +177,58 @@ void initelements() {
 		el_list.E_ELEMENTS[i_setpos_index + 1] = b_buttonlist[i_setpos_index + 1];
 	}
 }
-void editclick(_CPCD_SPRITE * s_look) {
+
+void editclick(_CPCD_SPRITE * s_look) 
+{
 	_CPCD_CLEAR(s_look, (_CPCD_COLOR) { 255, 255, 255 });
 
 }
-void edithover(_CPCD_SPRITE * s_look) {
+
+void edithover(_CPCD_SPRITE * s_look) 
+{
 	_CPCD_CLEAR(s_look, (_CPCD_COLOR) { 100, 100, 100 });
 
 }
-void editidle(_CPCD_SPRITE * s_look) {
+
+void editidle(_CPCD_SPRITE * s_look) 
+{
 	_CPCD_CLEAR(s_look, (_CPCD_COLOR) { 33, 33, 33 });
 }
-int callupdate() {
 
-	_CPCD_CLEAR(&s_graphwindow, (_CPCD_COLOR) { 100, 100, 100 });
+int callupdate() 
+{
+	printf("PING\n");
+	_C_POINT * p_dataset = NULL;
+	int i_datalength;
+
+	getdata_a(cs_loadedsample, us_periods, us_upscale,&p_dataset,&i_datalength);
+	
+	_CPCD_CLEAR(&s_graphwindow, _P_GRAPHCOLOR_BACKCOLOR);
 
 
+	_CPCD_DRAWLINE(&s_graphwindow, (_CPCD_VECTOR) {0, _P_GRAPHZERO}, (_CPCD_VECTOR) { _P_WIDTH, _P_GRAPHZERO}, _P_GRAPHCOLOR_ZEROLINE);
 
-	_CPCD_DRAWLINE(&s_graphwindow, (_CPCD_VECTOR) {0, _P_GRAPHZERO_AMPERE}, (_CPCD_VECTOR) { _P_WIDTH, _P_GRAPHZERO_AMPERE}, _P_GRAPHCOLOR_ZEROLINE);
-	_CPCD_DRAWLINE(&s_graphwindow, (_CPCD_VECTOR) {0, _P_GRAPHZERO_LOAD}, (_CPCD_VECTOR) { _P_WIDTH, _P_GRAPHZERO_LOAD}, _P_GRAPHCOLOR_ZEROLINE);
-	_CPCD_DRAWLINE(&s_graphwindow, (_CPCD_VECTOR) {0, _P_GRAPHZERO_INDUCTOR}, (_CPCD_VECTOR) { _P_WIDTH, _P_GRAPHZERO_INDUCTOR}, _P_GRAPHCOLOR_ZEROLINE);
 	
 	_CPCD_DRAWLINE(&s_graphwindow, (_CPCD_VECTOR) { _P_BUTTON_PADDING, 0}, (_CPCD_VECTOR) { _P_BUTTON_PADDING, _P_HEIGHT_S}, _P_GRAPHCOLOR_ZEROLINE);
+
+	register _CPCD_VECTOR v_posvec,v_lastpos;
+
+	for (int i_cdindex = 0; i_cdindex < i_datalength; i_cdindex++)
+	{
+		if (i_cdindex == 0)v_lastpos = (_CPCD_VECTOR) { _P_BUTTON_PADDING , _P_GRAPHZERO};
+
+		v_posvec.X = (int)(p_dataset[i_cdindex].d_t*us_draw_scale_x);
+		v_posvec.Y = -(int)(p_dataset[i_cdindex].d_i*us_draw_scale_y);
+
+		v_posvec.X += _P_BUTTON_PADDING;
+		v_posvec.Y += _P_GRAPHZERO;
+
+		printf("%d %d\n", v_posvec.X, v_posvec.Y);
+
+		_CPCD_DRAWLINE(&s_graphwindow, v_posvec, v_lastpos, _P_GRAPHCOLOR_POINTS);
+
+		v_lastpos = v_posvec;
+	}
 
 	return 0;
 }
